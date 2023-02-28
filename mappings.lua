f8e723fe518ba43d76040d0ab933d2c4393a960c
Dillon Townsend <townsenddillon.dt@gmail.com>
2023-02-28 15:52:52 -0600

    changed vertical movement mapping

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,8 @@ local M = {}
 M.general = {
   n = {
     -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true }),
+    vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
   },
 }
 
