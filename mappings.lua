2ea61103f93023739cd6f18b08022188fc6be9fd
Dillon Townsend <townsenddillon.dt@gmail.com>
2023-03-24 10:02:43 -0500

    remapped floating diagnostic

--- mappings.lua
+++ mappings.lua
@@ -1,12 +1,34 @@
 ---@type MappingsConfig
 local M = {}
 
+M.disabled = {
+  n = {
+    ["<leader>f"] = {
+      function()
+        vim.diagnostic.open_float()
+      end,
+      "floating diagnostic",
+    },
+  },
+}
+
 M.general = {
   n = {
     ["<C-d>"] = { "<C-d>zz", "half page down, centered" },
     ["<C-u>"] = { "<C-u>zz", "half page up, centered" },
     ["n"] = { "nzz", "next search result, centered" },
     ["N"] = { "Nzz", "next search result, centered" },
+  },
+}
+
+M.lspconfig = {
+  n = {
+    ["<leader>fd"] = {
+      function()
+        vim.diagnostic.open_float()
+      end,
+      "floating diagnostic",
+    },
   },
 }
 
